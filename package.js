24169515b4e6f8a433bff1f72dceb97193d1e05b
Rvach <Rvach.Flyver@gmail.com>
2017-03-13 22:36:01 +0300

    Update package.js

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'ElasticSearch utility wrapper for mongoDB integration and sync',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/toystars/elasticsearch-sync',
+  git: 'https://github.com/Rvach/elasticsearch-sync',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
